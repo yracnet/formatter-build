3bb6c13bfae895a6a5f4817420718e6b469f48a1
Willyams Yujra <yracnet@gmail.com>
2020-01-31 08:49:22 -0400

    * rename formatter build

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # formatter
 Format Source Code JAVA, CSS, JS, XML, HTML with Eclipse Platform
+
+
+Formatter Help is an API as utility based in https://code.revelc.net/formatter-maven-plugin
+
+What is the difference?
+
+1.- formatter-maven-plugin is an wrap of eclipse jdt core for maven-plugin and required that used in a pom.xml 
+
+2.- formatter-help not depend of maven-plugin, this could use as a simple LIB and wrap the eclipse format jdt
+
+You can use this api in your application in runtime without include the maven-plugin dependency
+
+This is simple:
+
+FormatterBuild build = FormatterBuild.create();
+build.setBasedir(basedir); //basedir is a root directory when exist a source code as JAVA, JS, HTML, CSS
+build.setDirectories(directory1, directory2); // this is necesary for declare all directory that format, remember that directory1, directory2 is a subdirectory of basedir
+build.execute();  //This execute the format code
+
+
+The FormatterBuild Class has many method for configure the execution like a https://code.revelc.net/formatter-maven-plugin
+
--- formatter-build/src/main/java/dev/yracnet/formatter/FormatterBuild.java
+++ formatter-build/src/main/java/dev/yracnet/formatter/FormatterBuild.java
@@ -16,115 +16,248 @@ import net.revelc.code.formatter.LineEnding;
  * @author wyujra
  */
 public interface FormatterBuild {
-
+/**
+ * Create the default instance for FormatterBuild
+ * @return 
+ */
     public static FormatterBuild create() {
-        FormatterBuild  build = new FormatterBuildImpl();
+        FormatterBuild build = new FormatterBuildImpl();
         build.setLog(new FormatterLogImpl());
         return build;
     }
 
+    /**
+     * Execute the format code
+     *
+     * @throws FormatterException
+     */
     public void execute() throws FormatterException;
 
     public FormatterLog getLog();
 
+    /**
+     * Set the Logger Implement
+     *
+     * @param logger
+     */
     public void setLog(FormatterLog logger);
 
     public File getTargetDirectory();
 
+    /**
+     * Set the Target Directory where generated the resumen and cache properties
+     *
+     * @param targetDirectory
+     */
     public void setTargetDirectory(File targetDirectory);
 
     public File getBasedir();
 
+    /**
+     * Set the base directory
+     *
+     * @param basedir
+     */
     public void setBasedir(File basedir);
 
     public File[] getDirectories();
 
+    /**
+     * Set sub directory of {basedir} for format
+     *
+     * @param directories
+     */
     public void setDirectories(File... directories);
 
     public String[] getIncludes();
 
+    /**
+     * Set the criterial include format files
+     *
+     * @param includes
+     */
     public void setIncludes(String... includes);
 
     public String[] getExcludes();
 
+    /**
+     * Set the criterial exclude format files
+     *
+     * @param excludes
+     */
     public void setExcludes(String... excludes);
 
     public String getCompilerSource();
 
+    /**
+     * Set the Version Java Source
+     *
+     * @param compilerSource
+     */
     public void setCompilerSource(String compilerSource);
 
     public String getCompilerCompliance();
 
+    /**
+     * Set the Version Java Compile
+     *
+     * @param compilerCompliance
+     */
     public void setCompilerCompliance(String compilerCompliance);
 
     public String getCompilerTargetPlatform();
 
+    /**
+     * Set the Version Target Platform
+     *
+     * @param compilerTargetPlatform
+     */
     public void setCompilerTargetPlatform(String compilerTargetPlatform);
 
     public Charset getEncodingAsCharset();
-    
+
     public String getEncoding();
 
+    /**
+     * Set Encoding Files
+     *
+     * @param encoding
+     */
     public void setEncoding(String encoding);
 
     public LineEnding getLineEnding();
 
+    /**
+     * Set the Line End character for WIN, LNX o MAC
+     *
+     * @param lineEnding
+     */
     public void setLineEnding(LineEnding lineEnding);
 
     public String getConfigJavaFile();
 
+    /**
+     * Set the File XML format eclipse Java Files
+     *
+     * @param configJavaFile
+     */
     public void setConfigJavaFile(String configJavaFile);
 
     public String getConfigJsFile();
 
+    /**
+     * Set the File XML format eclipse JS Files
+     *
+     * @param configJsFile
+     */
     public void setConfigJsFile(String configJsFile);
 
     public String getConfigHtmlFile();
 
+    /**
+     * Set the File Properties format eclipse HTML Files
+     *
+     * @param configHtmlFile
+     */
     public void setConfigHtmlFile(String configHtmlFile);
 
     public String getConfigXmlFile();
 
+    /**
+     * Set the File Properties format eclipse XML Files
+     *
+     * @param configXmlFile
+     */
     public void setConfigXmlFile(String configXmlFile);
 
     public String getConfigJsonFile();
 
+    /**
+     * Set the File Properties format eclipse JSON Files
+     *
+     * @param configJsonFile
+     */
     public void setConfigJsonFile(String configJsonFile);
 
     public String getConfigCssFile();
 
+    /**
+     * Set the File Properties format eclipse CSS Files
+     *
+     * @param configCssFile
+     */
     public void setConfigCssFile(String configCssFile);
 
     public boolean isSkipJavaFormatting();
 
+    /**
+     * Set Skip format Java files
+     *
+     * @param skipJavaFormatting
+     */
     public void setSkipJavaFormatting(boolean skipJavaFormatting);
 
     public boolean isSkipJsFormatting();
 
+    /**
+     * Set Skip format JS files
+     *
+     * @param skipJsFormatting
+     */
     public void setSkipJsFormatting(boolean skipJsFormatting);
 
     public boolean isSkipHtmlFormatting();
 
+    /**
+     * Set Skip format HTML files
+     *
+     * @param skipHtmlFormatting
+     */
     public void setSkipHtmlFormatting(boolean skipHtmlFormatting);
 
     public boolean isSkipXmlFormatting();
 
+    /**
+     * Set Skip format XML files
+     *
+     * @param skipXmlFormatting
+     */
     public void setSkipXmlFormatting(boolean skipXmlFormatting);
 
     public boolean isSkipJsonFormatting();
 
+    /**
+     * Set Skip format JSON files
+     *
+     * @param skipJsonFormatting
+     */
     public void setSkipJsonFormatting(boolean skipJsonFormatting);
 
     public boolean isSkipCssFormatting();
 
+    /**
+     * Set Skip format CSS files
+     *
+     * @param skipCssFormatting
+     */
     public void setSkipCssFormatting(boolean skipCssFormatting);
 
     public boolean isSkipFormatting();
 
+    /**
+     * Set Skip format all files
+     *
+     * @param skipFormatting
+     */
     public void setSkipFormatting(boolean skipFormatting);
 
     public boolean isUseEclipseDefaults();
 
+    /**
+     * Set TRUE for use the default format config of Eclipse
+     *
+     * @param useEclipseDefaults
+     */
     public void setUseEclipseDefaults(boolean useEclipseDefaults);
 
 }
